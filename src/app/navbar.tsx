15c599423cb1a727f1918ab26dd2252b8f7f5b0c
Sri <srivprasanna@gmail.com>
2023-04-30 17:26:50 -0400

    Added in animated navbar, still working one edits

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -2,27 +2,32 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-900 text-white flex items-center justify-between px-4 py-3">
+    <nav className="absolute bg-transperant text-white flex items-center justify-between px-6 py-3 w-full top-3">
       <div className="flex items-center">
-        <Link href="/">
-        </Link>
+        <Link href="/"></Link>
         <h1 className="ml-2 text-lg font-bold">Company Name</h1>
       </div>
       <ul className="flex items-center">
         <li className="ml-6">
-          <Link href="/">
-            <a className="hover:text-gray-400">Home</a>
-          </Link>
+          <button className="bg-transparent hover:bg-slate-50 hover:bg-opacity-5 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+            <Link href="/" className="hover:text-slate-100">
+              Home
+            </Link>
+          </button>
         </li>
         <li className="ml-6">
-          <Link href="/about">
-            <a className="hover:text-gray-400">About</a>
-          </Link>
+          <button className="bg-transparent hover:bg-slate-50 hover:bg-opacity-5 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+            <Link href="/about" className="hover:text-slate-100">
+              About
+            </Link>
+          </button>
         </li>
         <li className="ml-6">
-          <Link href="/contact">
-            <a className="hover:text-gray-400">Contact</a>
-          </Link>
+          <button className="bg-transparent hover:bg-slate-50 hover:bg-opacity-5 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+            <Link href="/contact" className="text-slate-100">
+              Contact
+            </Link>
+          </button>
         </li>
       </ul>
     </nav>
